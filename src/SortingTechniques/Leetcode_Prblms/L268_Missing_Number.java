f8dea040aaa3ef2ee9551af40b6d0a5125cd698d
Sudharshan Paul <sudarshanpaul@gmail.com>
2024-12-16 19:26:12 +0530

    Added some more methods

--- src/SortingTechniques/Leetcode_Prblms/L268_Missing_Number.java
+++ src/SortingTechniques/Leetcode_Prblms/L268_Missing_Number.java
@@ -6,8 +6,8 @@ public class L268_Missing_Number {
 
     public static void main(String[] args) {
 
-        int[] arr = {0,1};
-        System.out.println(missingNumber(arr));
+        int[] arr = {1};
+        System.out.println(missingNumber1(arr));
     }
 
     // https://leetcode.com/problems/missing-number/description/
@@ -23,6 +23,75 @@ public class L268_Missing_Number {
             }
         }
         return -1;
+    }
+
+    public static int missingNumber1(int[] nums){
+        int i=0;
+        int min = minOfArray(nums);
+        int max = maxOfArray(nums);
+        while(i<nums.length){
+            if(nums[i]==max){
+                i++;
+            }
+            else if(nums[i]-min != i){
+                swap(nums,nums[i]-min,i);
+            }else{
+                i++;
+            }
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=i){
+                return i;
+            }
+        }
+        return nums[nums.length-1]+1;
+    }
+
+
+    // This function is from [m,n] array not from 0
+    public static int missingNumber2(int[] nums){
+        int i=0;
+        int min = minOfArray(nums);
+        int max = maxOfArray(nums);
+        while(i<nums.length){
+            if(nums[i]==max){
+                i++;
+            }
+            else if(nums[i]-min != i){
+                swap(nums,nums[i]-min,i);
+            }else{
+                i++;
+            }
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]-min!=i){
+                return nums[i]-1;
+            }
+        }
+        return nums[nums.length-1]+1;
+    }
+    public static void swap(int[] arr,int first,int last){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+    public static int minOfArray(int[] arr){
+        int min = Integer.MAX_VALUE;
+        for(int num:arr){
+            if(num<min){
+                min = num;
+            }
+        }
+        return min;
+    }
+    public static int maxOfArray(int[] arr){
+        int max = Integer.MIN_VALUE;
+        for(int num:arr){
+            if(num>max){
+                max = num;
+            }
+        }
+        return max;
     }
 
 }
