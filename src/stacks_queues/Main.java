3d38e6e4a2b481688a7f9d8b095ab72661dca14e
sudharshanpaul <gantasudarshanpaul@gmail.com>
2025-05-10 21:16:42 +0530

    Started Stack and Queue Data Structures

---
+++ src/stacks_queues/Main.java
@@ -0,0 +1,7 @@
+package stacks_queues;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the folder for Stacks and Queues both theory and Leetcode sums");
+    }
+}
