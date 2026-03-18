175364652e6cda78002eb74c5c103a95fec369fe
sudharshanpaul <gantasudarshanpaul@gmail.com>
2026-03-18 18:37:48 +0530

    Leetcode: 102 - Binary Trees Level Order TRaversal

---
+++ src/My_Trees/leetcode_problems/L102_BinaryTree_BFS.java
@@ -0,0 +1,33 @@
+package My_Trees.leetcode_problems;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class L102_BinaryTree_BFS {
+
+    static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> innerList = new ArrayList<>();
+            int length = queue.size();
+            for(int i=0; i<length; i++){
+                TreeNode node = queue.pop();
+                innerList.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            list.add(innerList);
+        }
+        return list;
+    }
+}
