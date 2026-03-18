754fa9bf56a4b70db15744d3e7f24e09cff2edb4
sudharshanpaul <gantasudarshanpaul@gmail.com>
2026-03-18 18:46:00 +0530

    Leetcode: 637 - Average of Levels in Binary Tree

---
+++ src/My_Trees/leetcode_problems/L637_AverageOfLevelsinBinaryTree.java
@@ -0,0 +1,33 @@
+package My_Trees.leetcode_problems;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class L637_AverageOfLevelsinBinaryTree {
+
+    static void main(String[] args) {
+
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        List<Double> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        queue.add(root);
+        while(!queue.isEmpty()){
+            double sum = 0;
+            int length = queue.size();
+            for(int i=0; i<length; i++){
+                TreeNode node = queue.pop();
+                sum += node.val;
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            list.add(sum / length);
+        }
+        return list;
+    }
+}
