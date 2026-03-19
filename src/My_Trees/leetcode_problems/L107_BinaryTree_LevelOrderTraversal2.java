a4d70cb10c54571f1e27214853653a44cf14a36f
sudharshanpaul <gantasudarshanpaul@gmail.com>
2026-03-19 22:04:45 +0530

    Leetcode: 107 - Binary Tree Level Order Traversal 2

---
+++ src/My_Trees/leetcode_problems/L107_BinaryTree_LevelOrderTraversal2.java
@@ -0,0 +1,34 @@
+package My_Trees.leetcode_problems;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+public class L107_BinaryTree_LevelOrderTraversal2 {
+
+    static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) return result;
+
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int len = queue.size();
+            List<Integer> innerList = new ArrayList<>(len);
+            for(int i=0; i<len; i++){
+                TreeNode node = queue.pop();
+                innerList.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            result.add(0, innerList);
+        }
+
+        return result;
+    }
+}
