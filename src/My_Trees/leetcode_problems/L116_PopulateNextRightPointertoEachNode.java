40882b821b0e97943f9bfe1ac45dc276ac2cd84d
sudharshanpaul <gantasudarshanpaul@gmail.com>
2026-03-20 10:09:22 +0530

    Leetcode: 116 - Populate next pointer to Each Node

---
+++ src/My_Trees/leetcode_problems/L116_PopulateNextRightPointertoEachNode.java
@@ -0,0 +1,68 @@
+package My_Trees.leetcode_problems;
+
+import java.util.ArrayDeque;
+
+public class L116_PopulateNextRightPointertoEachNode {
+
+    static void main(String[] args) {
+
+    }
+
+    // Recursion -> Loop
+     public Node connect(Node root){
+         if(root == null) return null;
+
+         Node leftMost = root;
+
+         while(leftMost.left != null){
+             Node currentNode = leftMost;
+             while(currentNode != null){
+                 currentNode.left.next = currentNode.right;
+                 if(currentNode.next != null) currentNode.right.next = currentNode.next.left;
+                 currentNode = currentNode.next;
+             }
+             leftMost = leftMost.left;
+         }
+
+         return root;
+     }
+
+     // Using Recursion
+    public Node connect1(Node root){
+
+        if(root == null || root.left == null){
+            return root;
+        }
+
+        root.left.next = root.right;
+        if(root.next != null) root.right.next = root.next.left;
+        root.left = connect(root.left);
+        root.right = connect(root.right);
+
+        return root;
+
+    }
+
+
+    // Using Direct BFS approach
+     public Node connect2(Node root) {
+          ArrayDeque<Node> queue = new ArrayDeque<>();
+          if(root == null) return null;
+
+          queue.add(root);
+          while(!queue.isEmpty()){
+              int len = queue.size();
+              for(int i=0; i<len; i++){
+                  Node node = queue.pop();
+                  if(node.right != null){
+                      queue.add(node.left);
+                      queue.add(node.right);
+                  }
+                  if(i != len - 1) node.next = queue.getFirst();
+
+              }
+          }
+
+          return root;
+     }
+}
